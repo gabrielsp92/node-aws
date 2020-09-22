38af80ddbf91ba6f56d5de1e3d0ca932e71a866e
Gabriel_Soares <gsoaresp92@gmail.com>
2020-09-22 13:17:33 -0300

    added node scripts and configs

--- handler.js
+++ handler.js
@@ -1,15 +1,38 @@
 'use strict';
 
-module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+const AWS = require('aws-sdk');
+const sharp = require('sharp')
+const { basename, extname } = require('path')
+
+const S3 = new AWS.S3()
+
+module.exports.hello = async ({ Records: records }, context) => {
+  try {
+    await Promise.all(records.map(async record => {
+      const { key } = record.s3.object
+      const image = await S3.getObject({
+        Bucket: process.env.bucket,
+        key: key,
+      }).promise()
+
+      const optimized = await sharp(image.body)
+        .resize(1280, 720, { fit: 'inside', whitoutEnlargement: true })
+        .toFormat('jpeg', { progressive: true, quality: 50 })
+        .toBuffer()
+
+      await S3.putObject({
+        Body: optimized,
+        Bucket: process.env.bucket,
+        ContentType: 'image/jpeg',
+        key: `compressed/${basename(key, extname(key))}`,
+      }).promise()
+      
+    }));
+    return {
+      statusCode: 301,
+      body: {},
+    }
+  } catch (err) {
+    return err;
+  }
 };
